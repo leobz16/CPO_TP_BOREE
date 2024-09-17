78765159f4c44fd759d24707aa5f21fed4d83b3f
leoboree <leoboree@10.3.62.179>
2024-09-17 17:07:27 +0200

    j'ai fini l'exo 2 du TD0

---
+++ Exo 2/src/exo/pkg2/Exo2.java
@@ -0,0 +1,32 @@
+package exo.pkg2;
+
+/*
+ * BOREE Léo TDC 17/09/2024
+ */
+
+public class Exo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Déclaration des variables
+        int nb; // nombre d'entiers à additionner
+        int result; // résultat
+        int ind; // indice
+        
+        nb = 5;
+        result = 0;
+        
+        // Addition des nb premiers entiers
+        ind = 1;
+        while (ind <= nb) {
+            result = result + ind;
+            ind++; // Incrémenter l'indice pour éviter la boucle infinie
+        }
+        
+        // Affichage du résultat
+        System.out.println();
+        System.out.println("La somme des " + nb + " entiers est: " + result);
+    }
+}
