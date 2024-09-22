f6e3deae0ac9392d8b9b434a43c946675bed0790
leoboree <leoboree@10.3.62.179>
2024-09-22 21:43:08 +0200

    FIN Partie 3

--- Calculette/src/main/java/com/mycompany/calculette/Calculette.java
+++ Calculette/src/main/java/com/mycompany/calculette/Calculette.java
@@ -5,6 +5,8 @@
 
 package com.mycompany.calculette;
 
+import java.util.Scanner;
+
 /**
  *
  * BOREE Léo TDC 20/09/2024
@@ -12,20 +14,81 @@ package com.mycompany.calculette;
 public class Calculette {
 
     public static void main(String[] args) {
-        int nb; // nombre d'entiers à additionner
+       int nb;
 
-        System.out.println("\n Please enter the operator: ");
-        System.out.println("\n 1) add ");
-        System.out.println("\n 2) substract ");
-        System.out.println("\n 3) multiply ");
-        System.out.println("\n 4) divide ");
-        System.out.println("\n 5) modulo ");
-        nb=sc.nextInt(); // On demande a sc de donner le prochain entier
+       int operateur;
 
+       int first;
 
+       int second;
 
+       int result;
 
+    //nb=5;
 
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Please enter the operator:\n1) add\n2) substract\n3) multiply\n4) divide\n5) modulo");
+    nb=sc.nextInt(); // On demande a sc de donner le prochain entier
+    operateur = nb;
+
+    if (operateur > 5){
+            System.out.println("erreur");
+
+ 
+        }
+    if (operateur <=0){
+
+          System.out.println("erreur");
+
+       }
+
+    System.out.println("Please enter the first number:");
+    first=sc.nextInt();
+    System.out.println("Please enter the second number:");
+    second=sc.nextInt();
+    
+    if (operateur == 1){
+            result = first+second;
+            System.out.println("The result is :"+result+"");
+
+        }
+    if (operateur == 2){
+
+            result = first-second;
+           System.out.println("The result is :"+result+"");
+
+       
+
+        }
+ 
+    if (operateur == 3){
+
+            result = first*second;
+
+            System.out.println("The result is :"+result+"");
+
+       
 
     }
-}
+
+    if (operateur == 4){
+
+            result = first/second;
+
+            System.out.println("The result is :"+result+"");
+
+       
+
+        }
+
+    if (operateur == 5){
+
+            result = first%second;
+
+            System.out.println("The result is :"+result+"");
+
+        }
+        }
+    }
+
+ 
