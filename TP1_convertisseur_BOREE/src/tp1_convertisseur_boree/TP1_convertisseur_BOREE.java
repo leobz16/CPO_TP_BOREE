2103254f42f2f7fb74d1b5f237c348ed5a6ac0be
leoboree <leoboree@10.3.62.179>
2024-09-23 11:13:01 +0200

    Partie 2 TP1

--- TP1_convertisseur_BOREE/src/tp1_convertisseur_boree/TP1_convertisseur_BOREE.java
+++ TP1_convertisseur_BOREE/src/tp1_convertisseur_boree/TP1_convertisseur_BOREE.java
@@ -4,19 +4,47 @@
  */
 package tp1_convertisseur_boree;
 
+import java.util.Scanner;
+
 /**TP1 BOREE Léo 23/09/2024
 
  *
  * @author leoboree
  */
 public class TP1_convertisseur_BOREE {
-
-    /**
-     * @param args the command line arguments
-     */
+    public static double CelciusVersKelvin (double tCelcius) {
+        tCelcius = tCelcius + 273;  
+        return tCelcius;
+    }
+    public static double  KelvinVersCelcius(double tKelvin) {
+        tKelvin = tKelvin - 273;  
+        return tKelvin;
+    }
+    public static double  FarenheitVersCelcius(double tFarenheit ) {
+        tFarenheit = (tFarenheit - 32) * 0.55 ;  
+        return tFarenheit;   
+    }    
+    public static double  CelciusVersFarenheit(double celcius) {
+        celcius = (celcius * 0.55 ) + 32 ;  
+        return celcius;    
+    }
+    public static double   KelvinVersFarenheit(double kelvin) {
+        double celcius = KelvinVersCelcius ( kelvin);
+        double farenheit = CelciusVersFarenheit(celcius);
+        return farenheit;
+            
+    }
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        Scanner sc = new Scanner (System.in);
+            double nbre1;
+            double kelvin;
+            System.out.println("Saisissez une température ! "); 
+            nbre1=sc.nextDouble();
+            System.out.println("La température en celsius est de "+nbre1);
+            kelvin = nbre1+273;
+            System.out.println("La température en kelvin est de "+kelvin);
+
     }
     
 }
