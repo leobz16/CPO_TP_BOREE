072e97abee8b39cbd70bd4cd87b1ef6d5806607a
leoboree <leoboree@10.3.62.179>
2024-09-27 15:00:07 +0200

    Partie 2 TP1 Fini

--- TP1_convertisseur_BOREE/src/tp1_convertisseur_boree/TP1_convertisseur_BOREE.java
+++ TP1_convertisseur_BOREE/src/tp1_convertisseur_boree/TP1_convertisseur_BOREE.java
@@ -21,30 +21,77 @@ public class TP1_convertisseur_BOREE {
         return tKelvin;
     }
     public static double  FarenheitVersCelcius(double tFarenheit ) {
-        tFarenheit = (tFarenheit - 32) * 0.55 ;  
+        tFarenheit = (tFarenheit - 32) * 1.8 ;  
         return tFarenheit;   
     }    
     public static double  CelciusVersFarenheit(double celcius) {
-        celcius = (celcius * 0.55 ) + 32 ;  
+        celcius = (celcius * 1.8 ) + 32 ;  
         return celcius;    
     }
     public static double   KelvinVersFarenheit(double kelvin) {
         double celcius = KelvinVersCelcius ( kelvin);
         double farenheit = CelciusVersFarenheit(celcius);
         return farenheit;
-            
+     
     }
+    public static double FarenheitVersKelvin (double  Farenheit) {
+         double celsius = FarenheitVersCelcius(Farenheit);
+         double Kelvin = CelciusVersFarenheit(celsius);
+         return Kelvin;
+
+    }     
+    
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner (System.in);
-            double nbre1;
-            double kelvin;
-            System.out.println("Saisissez une température ! "); 
-            nbre1=sc.nextDouble();
-            System.out.println("La température en celsius est de "+nbre1);
-            kelvin = nbre1+273;
-            System.out.println("La température en kelvin est de "+kelvin);
+            
+        
+        double nombre;
+        double choix;
+        double CeKe;
+        double KeCe;
+        double FaCe;
+        double CeFa;
+        double KeFa;
+        double FaKe;
+       
+        System.out.println("Bonjour, saisissez une température :");
+        nombre = sc.nextInt();
+        System.out.println("Saisissez la conversion que vous souhaiter effectuer :");
+        
+        System.out.println("1) De Celcius vers Kelvin");
+        System.out.println("2) De Kelvin vers Celcius ");
+        System.out.println("3) De Farenheit vers Celcius ");
+        System.out.println("4) De Celcius vers Farenheit ");
+        System.out.println("5) De Kelvin vers Farenheit ");
+        System.out.println("6) De Farenheit vers Kelvin");
+        
+        choix = sc.nextInt();
+        CeKe = CelciusVersKelvin (nombre);
+        KeCe = KelvinVersCelcius(nombre) ;
+        FaCe = FarenheitVersCelcius(nombre);
+        CeFa = CelciusVersFarenheit(nombre);
+        KeFa = KelvinVersFarenheit(nombre);
+        FaKe = FarenheitVersKelvin(nombre);
 
+         if (choix == 1){
+            System.out.println(nombre+" degre Celcius est egal a "+CeKe+" deges Kelvin");
+         }
+         if (choix == 2){
+            System.out.println(nombre+" degre Kelvin est egal a "+KeCe+" deges Celcius");
+         }
+         if (choix == 3){
+            System.out.println(nombre+" degre Farenheit est egal a "+FaCe+" deges Celcius");
+         }
+         if (choix == 4){
+             System.out.println(nombre+" degre Celcius est egal a "+CeFa+" deges Farenheit");            
+         }
+          if (choix == 5){
+             System.out.println(nombre+" degre Kelvin est egal a "+KeFa+" deges Farenheit");         
+         }
+           if (choix == 6){
+              System.out.println(nombre+" degre Farenheit est egal a "+FaKe+" deges Kelvin");            
+         }
     }
-    
+
 }
