33468c2a1e5d44bb2dd85eae6e869895d00c241b
leoboree <leoboree@10.3.62.179>
2024-10-16 12:56:38 +0200

    TP2

---
+++ TP2_Bieres_BOREE/src/bieres_darties/BouteilleBiere.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bieres_darties;
+
+/**
+ *
+ * @author leoboree
+ */
+public class BouteilleBiere {
+    String nom ;
+    float degreAlcool ;
+    String brasserie ;
+    bool ouverte ;
+    
+    public void lireEtiquette() {
+    System.out.println("Bouteille de " + nom +" (" + degreAlcool +  " degres) \nBrasserie : " + brasserie ) ;
+
+}
+}
