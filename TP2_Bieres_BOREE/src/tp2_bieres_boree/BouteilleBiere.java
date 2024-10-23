d13b175121c1d90e7fecff712f0df91ef9ab9597
leoboree <leoboree@10.3.62.179>
2024-10-23 12:25:27 +0200

    TP2 Exo 1 fini

--- TP2_Bieres_BOREE/src/tp2_bieres_boree/BouteilleBiere.java
+++ TP2_Bieres_BOREE/src/tp2_bieres_boree/BouteilleBiere.java
@@ -2,12 +2,61 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package tp2_bieres_boree;
 
 /**
  *
  * @author leoboree
  */
+package tp2_bieres_boree;
+
+/**
+ *
+ * @author eliot
+ */
 public class BouteilleBiere {
-    
+
+    String nom;
+    float degreAlcool;
+    String brasserie;
+    boolean ouverte;
+
+    public BouteilleBiere(String unNom, float unDegre, String uneBrasserie) {
+        nom = unNom;
+        degreAlcool = unDegre;
+        brasserie = uneBrasserie;
+        ouverte = false;
+       
+    }
+
+    public void lireEtiquette() {
+
+        System.out.println("Bouteille de " + nom + " (" + degreAlcool
+                + " degres) \nBrasserie : " + brasserie);
+
+    }
+
+    public boolean Decapsuler() {
+        if (ouverte) {
+            System.out.println("Erreur : bière déjà ouverte");
+            return false;
+        } else {
+            ouverte = true;
+            System.out.println("La biere " + nom + " est maintenant ouverte.");
+            return true;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        String chaine_a_retourner;
+        chaine_a_retourner = nom + " (" + degreAlcool + " degres)Ouverte ? ";
+        if (ouverte == true) {
+            chaine_a_retourner += "oui";
+        } else {
+            chaine_a_retourner += "non";
+        }
+        return chaine_a_retourner;
+    }
+
 }
