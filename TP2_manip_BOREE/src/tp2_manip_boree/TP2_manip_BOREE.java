320d35328a06f3aa871c0cbc44769dfc11074cd3
leoboree <leoboree@10.3.62.179>
2024-11-05 17:11:50 +0100

    Fin TP2 Exo3

--- TP2_manip_BOREE/src/tp2_manip_boree/TP2_manip_BOREE.java
+++ TP2_manip_BOREE/src/tp2_manip_boree/TP2_manip_BOREE.java
@@ -15,6 +15,13 @@ public class TP2_manip_BOREE {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Tartiflette assiette1 = new Tartiflette(500) ;
+        Tartiflette assiette2 = new Tartiflette(600) ;
+        Tartiflette assiette3 = assiette2 ; 
+        System.out.println("nb de calories de Assiette 2 : " +
+        assiette2.nbCalories) ;
+        System.out.println("nb de calories de Assiette 3 : " +
+        assiette3.nbCalories) ;
     }
     
 }
